d01552025fe523a873317ffcd940d7313df9b2c1
mechanic-Q <46895416+mechanic-Q@users.noreply.github.com>
2019-07-19 01:41:32 +0800

    升级项目，添加自动打开浏览器、更新自动重启和静态资源处理等功能

---
+++ app.js
@@ -0,0 +1,11 @@
+const Koa = require('koa');
+const opn = require('opn');
+const fs = require('fs');
+const app = new Koa();
+const path = require('path');
+const KoaStatic = require('koa-static');
+app.use(KoaStatic('./'));
+
+app.listen(8000);
+opn('http://localhost:8000/')
+console.log('恭喜你，服务器启动成功：复制 http://localhost:8000/ 到浏览器即可访问');
