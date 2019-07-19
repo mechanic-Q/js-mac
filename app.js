11e1d43d49a25fea785b07b513257f3b81401fe6
mechanic-Q <46895416+mechanic-Q@users.noreply.github.com>
2019-07-19 18:55:18 +0800

    整理项目目录

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const app = new Koa();
 const path = require('path');
 const KoaStatic = require('koa-static');
-app.use(KoaStatic('./'));
+app.use(KoaStatic('./static'));
 
 app.listen(8000);
 opn('http://localhost:8000/')
--- index.js
+++
@@ -1,9 +0,0 @@
-const http = require("http");
-
-http.createServer(function (request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
-
-    response.end('写代码很快乐\n');
-}).listen(8888);
-
-console.log('Server running at http://localhost:8888/ ');
